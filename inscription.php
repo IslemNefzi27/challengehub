41d64ae676a2d06a0b14fbec6e0b3c86ce3d1f59
adaissimay <adaissi.may@gmail.com>
2026-02-28 17:47:36 +0100

    modification connexion.php

--- inscription.php
+++ inscription.php
@@ -3,7 +3,14 @@ require('configuration.php');
 $nom = $_POST['nom'];
 $email = $_POST['email'];
 $password = $_POST['motdepasse'];
-$req="INSERT INTO inscription(name,email,password) VALUES('$nom','$email','$password')";
+$confirm_password = $_POST['confirm_password'];
+if($password !== $confirm_password){
+    echo"<div>
+    <p>Les mots de passe ne correspondent pas.</p>
+    </div>";
+    exit();
+}
+$req="INSERT INTO user(nom_utilisateur,email_utilisateur,mot_passe) VALUES('$nom','$email','$password')";
 $res=mysqli_query($conn,$req);
 if($res){
     echo"<div>
