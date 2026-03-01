21f085dbc0372156394b785c4b5d802eee20ffb1
adaissimay <adaissi.may@gmail.com>
2026-03-01 03:14:46 +0100

    added usermodel and modifications

--- app/controle/connexion.php
+++ app/controle/connexion.php
@@ -1,40 +1,22 @@
 <?php
-require('configuration.php');
+require("../app/models/usermodel.php");
 session_start();
-if(isset($_SESSION['email']) && isset($_SESSION['motdepasse'])){
-    header('Location: challenges.html');
-    exit();
-}
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
+$usermodel=new usermodel($conn);
 $email=$_POST['email'];
-$password=$_POST['motdepasse'];}
-
-if(empty($email) || empty($password)){
-    echo"<div>
-    <p>Veuillez remplir tous les champs.</p>
-    <a href='connexion.html'>Retour à la connexion</a>
-    </div>";
-    exit();
-}
-if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    echo"<div>
-    <p>Adresse e-mail invalide. Veuillez entrer une adresse e-mail valide.</p>
-    <a href='connexion.html'>Retour à la connexion</a>
-    </div>";
-    exit();
-}
-$req="SELECT * FROM user WHERE email_utilisateur='$email' AND mot_passe='$password'";
-$res=mysqli_query($conn,$req);
-if($res){
-    if(mysqli_num_rows($res)==1){
-        echo"<div>
-        <p>Connexion réussie. Bienvenue, $email!</p>
-        <a href='challenges.html'>Accéder à la page de défis!</a>
-        </div>";
+$password=$_POST['motdepasse'];
+$user=$usermodel->trouvepemail($email);
+if($user){
+    if(password_verify($password, $user['mot_passe']))
+        {$_SESSION['email']=$email;
+        header('Location: challenge.php');
+        exit();
     }else{
-        echo"<div>
-        <p>Adresse e-mail ou mot de passe incorrect. Veuillez réessayer.</p>
-        <a href='connexion.html'>Retour à la connexion</a>
-        </div>";
+        header('Location: connexion.html');
+        exit();
     }
 }
+else{
+    header('Location: connexion.html');
+    exit();
+}
+?>
--- app/controle/inscription.php
+++ app/controle/inscription.php
@@ -1,25 +1,37 @@
 <?php
-require('configuration.php');
+require('../models/UserModel.php');
+$usermodel=new usermodel($conn);
 $nom = $_POST['nom'];
 $email = $_POST['email'];
 $password = $_POST['motdepasse'];
 $confirm_password = $_POST['confirm_password'];
-if($password !== $confirm_password){
+if( empty($nom)||empty($email)||empty($password)||empty($confirm_password)){
     echo"<div>
-    <p>Les mots de passe ne correspondent pas.</p>
-    </div>";
-    exit();
-}
-$req="INSERT INTO user(nom_utilisateur,email_utilisateur,mot_passe) VALUES('$nom','$email','$password')";
-$res=mysqli_query($conn,$req);
+    <p>Veuillez remplir tous les champs</p>
+    exit();";
+}//remplissage des champs
+if($password!=$confirm_password){
+    echo"<div>
+    <p>Les mots de passe ne correspondent pas. Veuillez réessayer.</p>
+    exit();";
+}//password confirmation
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+//hashage du mot de passe
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+   echo"<div>
+   <p>Adresse e-mail invalide. Veuillez entrer une adresse e-mail valide.</p>
+    exit();";
+}//validation d'email
+if($usermodel->emailexistant($email)){
+    echo"<div>
+    <p>Adresse e-mail déjà utilisée</p>
+    exit();";
+}//validation d'email existant
+$res=$usermodel->creation($nom,$email,$hashed_password);
 if($res){
     echo"<div>
-    <p>Votre inscription a été effectuée avec succès.</p>
-    <a href='connexion.php'>se connecter</a>
-    </div>";
-}else{
-    echo"<div>
-    <p>Une erreur s'est produite lors de l'inscription. Veuillez réessayer.</p>
-    <a href='inscription.html'>Retour à l'inscription</a>
-    </div>";
-}
+    <p>Inscription réussie. Bienvenue, $nom!</p>
+    <a href='connexion.html'>Accéder à la page de connexion!</a>
+    </div>";}
+    else{die("erreur de s'inscrire");}
+?>
