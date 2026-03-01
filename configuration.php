67489ebf6a7fdc1bb9f1806618b386b1495d7890
adaissimay <adaissi.may@gmail.com>
2026-03-01 01:01:13 +0100

    added app folder from desktop

--- configuration.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('serveur', 'localhost');
-define('utilisateur', 'root');  
-define('motdepasse', '');
-define('basededonnees', 'gestion_des_utilisateurs');
-$conn = mysqli_connect(serveur, utilisateur, motdepasse, basededonnees);
-if (!$conn) {
-    die("La connexion a échoué: " . mysqli_connect_error());
-}
